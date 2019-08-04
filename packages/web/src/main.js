751010dbb23fa1ac0329a74641e1cd2eff8c3308
Joe-noh <goflb.jh@gmail.com>
2019-08-04 17:38:51 +0900

    change routing library
    crayon to svelte-routing

--- packages/web/src/main.js
+++ packages/web/src/main.js
@@ -1,14 +1,3 @@
-import crayon from 'crayon'
-import { router } from 'crayon-svelte'
+import App from './App.svelte'
 
-import Top from './pages/Top.svelte'
-import Signup from './pages/Signup.svelte'
-
-const app = crayon.create()
-
-app.use(router())
-
-app.path('/', (req, res) => res.mount(Top))
-app.path('/signup', (req, res) => res.mount(Signup))
-
-app.load()
+const app = new App({ target: document.body })
