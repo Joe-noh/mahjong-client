07d553fe1c0309d54d4da9ddfb0eeb210afa3cfd
Joe-noh <goflb.jh@gmail.com>
2019-08-11 14:16:20 +0900

    change route structure

--- packages/web/src/App.svelte
+++ packages/web/src/App.svelte
@@ -1,22 +1,17 @@
 <script>
   import { onMount } from 'svelte'
-  import { Router, Link, Route } from 'svelte-routing'
-  import { session } from '@/stores/session'
-  import Top from './pages/Top.svelte'
-  import Login from './pages/Login.svelte'
-
-  export let url = ''
-
-  onMount(() => session.restore())
+  import { Router, Link, Route, navigate } from 'svelte-routing'
+  import Top from '@/pages/Top.svelte'
+  import AuthenticatedRoute from '@/pages/AuthenticatedRoute.svelte'
 </script>
 
-<Router url="{url}">
+<Router>
   <nav>
     <Link to="/">Home</Link>
-    <Link to="/login">Login</Link>
+    <Link to="/lobby">Login</Link>
   </nav>
   <div>
-    <Route path="/"><Top /></Route>
-    <Route path="/login" component="{Login}" />
+    <Route path="/" component="{Top}" />
+    <Route path="/*" component="{AuthenticatedRoute}" />
   </div>
 </Router>
---
+++ packages/web/src/pages/AuthenticatedRoute.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { onMount } from 'svelte'
+  import { Router, Route, navigate } from 'svelte-routing'
+  import { session, isLoggedIn } from '@/stores/session'
+  import Lobby from '@/pages/Lobby.svelte'
+
+  onMount(() => {
+    session.restore()
+
+    if (!$isLoggedIn) {
+      navigate('/', { replace: true })
+    }
+  })
+</script>
+
+<Router>
+  <Route path="/lobby" component="{Lobby}" />
+</Router>
