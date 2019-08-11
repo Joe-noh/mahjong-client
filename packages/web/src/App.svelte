918b0ad6aa25f9db016d9896054a167033a42470
Joe-noh <goflb.jh@gmail.com>
2019-08-11 12:01:48 +0900

    change signup to login

--- packages/web/src/App.svelte
+++ packages/web/src/App.svelte
@@ -3,7 +3,7 @@
   import { Router, Link, Route } from 'svelte-routing'
   import { session } from '@/stores/session'
   import Top from './pages/Top.svelte'
-  import Signup from './pages/Signup.svelte'
+  import Login from './pages/Login.svelte'
 
   export let url = ''
 
@@ -13,10 +13,10 @@
 <Router url="{url}">
   <nav>
     <Link to="/">Home</Link>
-    <Link to="/signup">Signup</Link>
+    <Link to="/login">Login</Link>
   </nav>
   <div>
-    <Route path="/signup" component="{Signup}" />
     <Route path="/"><Top /></Route>
+    <Route path="/login" component="{Login}" />
   </div>
 </Router>
