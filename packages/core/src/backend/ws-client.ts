2a949992da2b0fac9a40b6f240fa9a1f6283d88e
Joe-noh <goflb.jh@gmail.com>
2019-08-13 20:47:12 +0900

    fix test

--- packages/core/src/backend/ws-client.ts
+++ packages/core/src/backend/ws-client.ts
@@ -2,17 +2,18 @@ import Phoenix from 'phoenix'
 import { decamelize } from '@ridi/object-case-converter'
 
 export class WSClient {
+  private url: string
+  private authToken: string
   private socket: Phoenix.Socket
   private channel: Phoenix.Channel
 
   public constructor(opts: { baseUrl: string; authToken: string }) {
-    const url = opts.baseUrl.replace(/^http/, 'ws').replace(/\/$/, '') + '/socket'
-    const params = decamelize({ authToken: opts.authToken })
-
-    this.socket = new Phoenix.Socket(url, { params })
+    this.url = opts.baseUrl.replace(/^http/, 'ws').replace(/\/$/, '') + '/socket'
+    this.authToken = opts.authToken
   }
 
   public connect(): void {
+    this.socket = new Phoenix.Socket(this.url, { params: decamelize({ authToken: this.authToken }) })
     this.socket.connect()
   }
 
