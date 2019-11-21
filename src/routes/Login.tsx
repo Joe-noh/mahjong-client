80620f8dd31a60be3a7d0fbf6c93444c52e185e9
Joe-noh <goflb.jh@gmail.com>
2019-11-21 23:47:28 +0900

    login as specified user

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -7,20 +7,25 @@ import { Session } from 'types'
 export const Login: React.FC = () => {
   const history = useHistory()
   const location = useLocation()
+  const { from } = location.state || { from: { pathname: '/new' } }
 
-  const login = async () => {
-    const { from } = location.state || { from: { pathname: '/new' } }
+  const loginAs = (userId: number) => {
+    return async () => {
+      const { token } = await api.post<Session>('/api/sessions', { userId })
+      session.setToken(token)
 
-    const { token } = await api.post<Session>('/api/guests', {})
-    session.setToken(token)
-
-    history.replace(from)
+      history.replace(from)
+    }
   }
+
+  const buttons = [1, 2, 3, 4].map(i => {
+    return <button onClick={loginAs(i)}>Login as User {i}</button>
+  })
 
   return (
     <div>
       <h1>Login</h1>
-      <button onClick={login}>Login as Guest</button>
+      {buttons}
     </div>
   )
 }
